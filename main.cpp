ef3c4aa5c2090f2102574b36b998afe148c7088b
Marisa Kirisame <lolisa@marisa.moe>
2022-09-06 07:43:04 -0600

    save

--- main.cpp
+++ main.cpp
@@ -1,52 +1,73 @@
-struct Record {
-  tick_t finished_tick;
-  Zombie<_> value;
-}
+#include <memory>
+#include <functional>
+#include <unordered_map>
+#include <vector>
+#include <optional>
+
+struct EZombieNode {
+  
+};
+
+using EZombie = std::shared_ptr<EZombieNode>;
+using WEZombie = std::weak_ptr<EZombieNode>;
+
+struct EComputerNode {
+  
+};
+
+using EComputer = std::shared_ptr<EComputerNode>;
+using WEComputer = std::weak_ptr<EComputerNode>;
 
 // start at 0.
-// a tick pass whenever a Zombie is created.
-using tick_t = int64_t;
+// a tock pass whenever a Computer is created.
+using tock_t = int64_t;
+
+struct Record {
+  tock_t finished_tock;
+  WEZombie value;
+};
 
 struct Scope {
-  tick_t &tick;
-  std::vector<Zombie<_>> created;
+  tock_t &tick;
+  std::vector<WEZombie> created;
 };
 
 struct Context {
   std::vector<Scope> scopes;
-  tick_t day, night;
-}
+  tock_t day, night;
+};
+
 struct World {
   static World& get_world();
   // maybe this should be an kinetic datastructure, we will see
-  std::vector<ERemat> evict_pool;
+  std::vector<WEComputer> evict_pool;
   // when replaying a function, look at recorded to repatch recursive remat, thus existing fragment can be reused
-  std::unordered_map<tick_t, Record> recorded;
+  std::unordered_map<tock_t, Record> recorded;
   Context ctx;
 };
 
 // does not recurse
 template<typename T>
-struct RematNode {
-  std::function<void(...)>;
-  std::vector<> input;
-  std::vector<> output;
+struct ComputerNode : EComputerNode {
+  std::function<void()> f;
+  std::vector<EZombie> input;
+  std::vector<WEZombie> output;
   size_t memory;
   int64_t compute_cost;
   time_t enter_time;
-  tick_t started_tick;
+  tock_t started_tock;
 };
 
 template<typename T>
-struct Remat {
-  std::shared_ptr<RematNode<T>> node;
+struct Computer {
+  std::shared_ptr<ComputerNode<T>> node;
 };
 
 template<typename T>
-struct ZombieNode {
+struct ZombieNode : EZombieNode {
   std::optional<T> t;
-  std::optional<Remat<T>> rm;
-  // invariant: t or rm must has_value()
+  std::optional<Computer<T>> com;
+  // invariant: t or com must has_value()
 };
 
 template<typename T>
