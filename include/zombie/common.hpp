6c9931dbf00668b9948eb04fb9c6f24650f66120
Marisa Kirisame <marisa@cs.utah.edu>
2023-04-12 16:10:38 -0600

    save

--- include/zombie/common.hpp
+++ include/zombie/common.hpp
@@ -1,1 +1,9 @@
 #pragma once
+
+template<typename A, typename B, typename C>
+decltype(std::declval<B>()()) bracket(const A& a, const B& b, const C& c) {
+  a();
+  auto ret = b();
+  c();
+  return ret;
+}
