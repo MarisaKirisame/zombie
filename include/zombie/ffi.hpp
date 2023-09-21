ce32f590e8b55698c26582c1a30e762b5f9cbacc
Wtz_LASR <wtz_lasr@outlook.com>
2023-09-22 06:29:39 +0800

    ffi pt.2

--- include/zombie/ffi.hpp
+++ include/zombie/ffi.hpp
@@ -3,52 +3,37 @@
 #include "heap/aff_function.hpp"
 #include "heap/kinetic.hpp"
 
-AffFunction *aff_function_new(slope_t slope, shift_t x_shift)
+extern "C"
 {
-    return new AffFunction(slope, x_shift);
-}
+    AffFunction *aff_function_new(slope_t slope, shift_t x_shift);
 
-aff_t aff_function_eval(const AffFunction *aff_function, shift_t x)
-{
-    return (*aff_function)(x);
-}
+    aff_t aff_function_eval(const AffFunction *aff_function, shift_t x);
 
-aff_t *aff_function_lt_until(const AffFunction *aff_function, const AffFunction *rhs)
-{
-    auto res = aff_function->lt_until(*rhs);
-    if (res)
-    {
-        aff_t *rres = new aff_t(*res);
-        return rres;
-    }
-    else
-    {
-        return nullptr;
-    }
-}
+    aff_t *aff_function_lt_until(const AffFunction *aff_function, const AffFunction *rhs);
 
-aff_t *aff_function_le_until(const AffFunction *aff_function, const AffFunction *rhs)
-{
-    auto res = aff_function->le_until(*rhs);
-    if (res)
-    {
-        aff_t *rres = new aff_t(*res);
-        return rres;
-    }
-    else
-    {
-        return nullptr;
-    }
-}
+    aff_t *aff_function_le_until(const AffFunction *aff_function, const AffFunction *rhs);
 
-struct EmptyNotifyHeapIndexChanged
-{
-    void operator()(const void *&n, const size_t &idx) {}
-};
+    struct EmptyNotifyHeapIndexChanged;
 
-using KineticHanger = HeapImpls::KineticMinHeap<void *, true, EmptyNotifyHeapIndexChanged>;
+    using KineticHanger = HeapImpls::KineticMinHeap<void *, true, EmptyNotifyHeapIndexChanged>;
 
-KineticHanger *kinetic_hanger_new(int64_t time)
-{
-    return new KineticHanger(time);
+    KineticHanger *kinetic_hanger_new(int64_t time);
+
+    size_t kinetic_hanger_size(const KineticHanger *hanger);
+
+    bool kinetic_hanger_empty(const KineticHanger *hanger);
+
+    void kinetic_hanger_insert(KineticHanger *hanger, const void *t, const AffFunction *aff);
+
+    void *kinetic_hanger_peek(KineticHanger *hanger);
+
+    void *kinetic_hanger_index(KineticHanger *hanger, size_t i);
+
+    void *kinetic_hanger_pop(KineticHanger *hanger);
+
+    void *kinetic_hanger_remove(KineticHanger *hanger, size_t i);
+
+    int64_t kinetic_hanger_time(const KineticHanger *hanger);
+
+    void kinetic_hanger_advance_to(KineticHanger *hanger, int64_t new_time);
 }
---
+++ src/ffi.cpp
@@ -0,0 +1,95 @@
+#include "zombie/ffi.hpp"
+
+AffFunction *aff_function_new(slope_t slope, shift_t x_shift)
+{
+    return new AffFunction(slope, x_shift);
+}
+
+aff_t aff_function_eval(const AffFunction *aff_function, shift_t x)
+{
+    return (*aff_function)(x);
+}
+
+aff_t *aff_function_lt_until(const AffFunction *aff_function, const AffFunction *rhs)
+{
+    auto res = aff_function->lt_until(*rhs);
+    if (res)
+    {
+        aff_t *rres = new aff_t(*res);
+        return rres;
+    }
+    else
+    {
+        return nullptr;
+    }
+}
+
+aff_t *aff_function_le_until(const AffFunction *aff_function, const AffFunction *rhs)
+{
+    auto res = aff_function->le_until(*rhs);
+    if (res)
+    {
+        aff_t *rres = new aff_t(*res);
+        return rres;
+    }
+    else
+    {
+        return nullptr;
+    }
+}
+
+struct EmptyNotifyHeapIndexChanged
+{
+    void operator()(void *n, size_t idx) {}
+};
+
+KineticHanger *kinetic_hanger_new(int64_t time)
+{
+    return new KineticHanger(time);
+}
+
+size_t kinetic_hanger_size(const KineticHanger *hanger)
+{
+    return hanger->size();
+}
+
+bool kinetic_hanger_empty(const KineticHanger *hanger)
+{
+    return hanger->empty();
+}
+
+void kinetic_hanger_insert(KineticHanger *hanger, const void *t, const AffFunction *aff)
+{
+    const void *&tmp = t;
+    hanger->insert(tmp, *aff);
+}
+
+void *kinetic_hanger_peek(KineticHanger *hanger)
+{
+    return hanger->peek();
+}
+
+void *kinetic_hanger_index(KineticHanger *hanger, size_t i)
+{
+    return (*hanger)[i];
+}
+
+void *kinetic_hanger_pop(KineticHanger *hanger)
+{
+    return hanger->pop();
+}
+
+void *kinetic_hanger_remove(KineticHanger *hanger, size_t i)
+{
+    return hanger->remove(i);
+}
+
+int64_t kinetic_hanger_time(const KineticHanger *hanger)
+{
+    return hanger->time();
+}
+
+void kinetic_hanger_advance_to(KineticHanger *hanger, int64_t new_time)
+{
+    return hanger->advance_to(new_time);
+}
