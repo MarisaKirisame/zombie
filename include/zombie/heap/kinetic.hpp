2568649b2a82c2252c41133eaa4d122d1dbcc5c8
Marisa Kirisame <marisa@cs.utah.edu>
2023-11-04 19:17:50 -0600

    turn on train

--- include/zombie/heap/kinetic.hpp
+++ include/zombie/heap/kinetic.hpp
@@ -333,7 +333,7 @@ public:
   };
 
   struct Train {
-    constexpr static bool use_train = false;
+    constexpr static bool use_train = true;
     constexpr static double threshold_factor = 3;
     constexpr static size_t enough_car = 2;
     constexpr static size_t max_car = 3;
