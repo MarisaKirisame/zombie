c6db1829dac3f339d8da12b5585435b081d9a1e2
ç® <2447746158@qq.com>
2023-12-25 02:05:13 +0800

    add unordered_map cache

--- include/zombie/tock/cache.hpp
+++ include/zombie/tock/cache.hpp
@@ -5,19 +5,47 @@
 #include <functional>
 #include <memory>
 #include <optional>
+#include <unordered_map>
 
 #include "common.hpp"
 
 namespace TockTreeCaches {
 
-template<typename Node>
-struct NoneCache {
-    std::optional<std::shared_ptr<Node>> get(const Tock& t) {
-        return {};
-    }
+template <typename Node> struct NoneCache {
+  std::optional<std::shared_ptr<Node>> get(const Tock &t) { 
+    return {}; 
+  }
 
-    void update(const Tock& t, const std::shared_ptr<Node>& p) {
+  void update(const Tock &t, const std::shared_ptr<Node> &p) {
+  }
+};
+
+template <typename Node> struct HashCache {
+  struct HashTock {
+    std::size_t operator()(const Tock &s) const {
+      return std::hash<int64_t>{}(s.tock);
     }
+  };
+
+  std::unordered_map<Tock, std::weak_ptr<Node>, HashTock> hash;
+
+  std::optional<std::shared_ptr<Node>> get(const Tock &t) {
+    auto iter = hash.find(t);
+    if (iter == hash.end()) {
+      return {};
+    } else {
+      std::weak_ptr<Node> ptr = iter->second;
+      if (ptr.expired()) {
+        return {};
+      } else {
+        return ptr.lock();
+      }
+    }
+  }
+
+  void update(const Tock &t, std::shared_ptr<Node> &p) {
+    hash[t] = std::weak_ptr<Node>(p);
+  }
 };
 
 }; // end of namespace TockTreeCaches
--- include/zombie/tock/tock.hpp
+++ include/zombie/tock/tock.hpp
@@ -15,6 +15,6 @@ template<TockTreeImpl impl, typename T, typename NotifyParentChanged>
 using TockTree = std::variant_alternative_t<
   (size_t)impl,
   std::variant<
-    TockTreeImpls::TockTree<T, TockTreeCaches::NoneCache, NotifyParentChanged>
+    TockTreeImpls::TockTree<T, TockTreeCaches::HashCache, NotifyParentChanged>
   >
 >;
